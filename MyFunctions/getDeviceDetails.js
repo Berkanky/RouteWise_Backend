4901c61f436ae0c0c4f6606048d8d96da1181282
Berkan <yusufbk92@gmail.com>
2025-04-25 01:23:04 +0300

    cihaz detayları servisi fix

--- MyFunctions/getDeviceDetails.js
+++ MyFunctions/getDeviceDetails.js
@@ -1,8 +1,7 @@
 const getDeviceDetails = (req, res) => {
   var ipAddress = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   var userAgent = req.headers["user-agent"];
-  var DeviceId = req.body.DeviceId || req.params.DeviceId;
-
+  var DeviceId = req.body?.DeviceId || req.params?.DeviceId || '';
   return { IPAddress: ipAddress, DeviceName: userAgent, Date: new Date(), DeviceId: DeviceId };
 };
 
