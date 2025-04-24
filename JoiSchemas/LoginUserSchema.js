ea914c2aefc08b3c7a4ed7e53b978c3e7ef5b887
Berkan <yusufbk92@gmail.com>
2025-04-25 02:36:22 +0300

    joi login fix

--- JoiSchemas/LoginUserSchema.js
+++ JoiSchemas/LoginUserSchema.js
@@ -10,15 +10,19 @@ const LoginUserSchema = Joi.object({
     }),
 
   Password: Joi.string()
-      .min(8)
-      .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])"))
-      .required()
-      .messages({
-        "string.min": "The password must be at least 8 characters.",
-        "string.pattern.base":
-          "The password must contain at least one uppercase letter, one lowercase letter and one number.",
-        "any.required": "The password is required.",
-      }),
+    .min(8)
+    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])"))
+    .required()
+    .messages({
+      "string.min": "The password must be at least 8 characters.",
+      "string.pattern.base":
+        "The password must contain at least one uppercase letter, one lowercase letter and one number.",
+      "any.required": "The password is required.",
+    }),
+
+  Verified: Joi.boolean().optional(),
+  VerifySended: Joi.boolean().optional(),
+  DeviceDetails: Joi.object().optional()
 });
 
 module.exports = LoginUserSchema;
