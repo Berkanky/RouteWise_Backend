e805408c4815565cd6a1cd28d209bbcbc291dded
Berkan <yusufbk92@gmail.com>
2025-04-25 13:22:52 +0300

    fix

--- MyFunctions/CalculateExpireDate.js
+++ MyFunctions/CalculateExpireDate.js
@@ -1,4 +1,4 @@
-const CalculateExpireDate = ({ hours = 0, minutes = 0 }) => {
+const CalculateExpireDate = ({ hours, minutes }) => {
     var hour_in_ms = 60 * 60 * 1000;
     var minute_in_ms = 60 * 1000;
     return new Date().getTime() + (hours * hour_in_ms) + (minutes * minute_in_ms);
