78fcbda1706a457cedf2d659055b0b16d17b6f72
Berkan <yusufbk92@gmail.com>
2025-04-25 14:59:52 +0300

    refresh token fix

--- InsertFunctions/CreateRefreshToken.js
+++ InsertFunctions/CreateRefreshToken.js
@@ -2,47 +2,48 @@ const CreateRefreshToken = require("../JWTModules/CreateRefreshToken");
 const RefreshToken = require("../Schemas/RefreshToken");
 const CalculateExpireDate = require("../MyFunctions/CalculateExpireDate");
 
-async function InsertRefreshToken(_id, CreatedSHA256RefreshToken){
+async function InsertRefreshToken(_id, CreatedRefreshTokenEncrypted) {
+  var newRefreshTokenObj = {
+    UserId: _id,
+    Token: CreatedRefreshTokenEncrypted,
+    ExpiredDate: CalculateExpireDate({ hours: 5, minutes: 0 }),
+  };
 
-    var newRefreshTokenObj = {
-        UserId: _id,
-        Token: CreatedSHA256RefreshToken,
-        ExpiredDate: CalculateExpireDate({ hours: 5, minutes: 0})
-    };
+  var newRefreshToken = new RefreshToken(newRefreshTokenObj);
+  var createdRefreshToken = await newRefreshToken.save();
+  return createdRefreshToken.Token;
+}
 
-    var newRefreshToken = new RefreshToken(newRefreshTokenObj);
-    var createdRefreshToken = await newRefreshToken.save();
-    return createdRefreshToken.Token
-};  
-
-async function DeleteExpiredRefreshToken(_id, ExpiredRefreshTokenId){
-
-    var filter = { UserId: _id, _id: ExpiredRefreshTokenId};
-    await RefreshToken.findOneAndDelete(filter);
-};
+async function DeleteExpiredRefreshToken(_id, ExpiredRefreshTokenId) {
+  var filter = { UserId: _id, _id: ExpiredRefreshTokenId };
+  await RefreshToken.findOneAndDelete(filter);
+}
 
 async function CreateRefreshTokenFunction(req, res, _id) {
+  var CreatedRefreshTokenEncrypted, CreatedRefreshTokenDecrypted;
 
-    var CreatedRefreshTokenObj = CreateRefreshToken();
-    var CreatedSHA256RefreshToken = CreatedRefreshTokenObj.RefreshTokenCrypted;
-    var CreatedRefreshTokenDecrypted = CreatedRefreshTokenObj.RefreshTokenDecrypted;
+  var refreshTokenFilter = { UserId: _id };
+  var refreshToken = await RefreshToken.findOne(refreshTokenFilter);
 
-    var refreshTokenFilter = { UserId: _id };
-    var refreshToken = await RefreshToken.findOne(refreshTokenFilter);
+  var CreatedRefreshTokenObj = CreateRefreshToken();
+  CreatedRefreshTokenEncrypted = CreatedRefreshTokenObj.RefreshTokenCrypted;
+  CreatedRefreshTokenDecrypted = CreatedRefreshTokenObj.RefreshTokenDecrypted;
 
-    var RefreshTokenAvaliable = false;
+  if (refreshToken) {
 
-    if( !refreshToken) await InsertRefreshToken(_id, CreatedSHA256RefreshToken);
+    await DeleteExpiredRefreshToken(_id, refreshToken._id.toString());
+    await InsertRefreshToken(_id, CreatedRefreshTokenEncrypted);
 
-    if( refreshToken && new Date() > new Date(String(refreshToken.ExpiredDate))) {
+  }else{
 
-        await DeleteExpiredRefreshToken(_id, refreshToken._id.toString());
-        await InsertRefreshToken(_id, CreatedSHA256RefreshToken);
-    }else{
-        RefreshTokenAvaliable = true;
-    }
+    await InsertRefreshToken(_id, CreatedRefreshTokenEncrypted);
+  }
 
-    return { RefreshTokenCrypted: RefreshTokenAvaliable ? refreshToken.Token  : CreatedSHA256RefreshToken, RefreshTokenDecrypted: CreatedRefreshTokenDecrypted}
-};  
+  return {
+
+    RefreshTokenCrypted: CreatedRefreshTokenEncrypted,
+    RefreshTokenDecrypted: CreatedRefreshTokenDecrypted,
+  };
+}
 
 module.exports = CreateRefreshTokenFunction;
