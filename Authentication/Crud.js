25d45f89bb04461d4877531981d55284114b7f47
Berkan <yusufbk92@gmail.com>
2025-05-12 01:44:44 +0300

    fix

--- Authentication/Crud.js
+++ Authentication/Crud.js
@@ -599,7 +599,7 @@ app.put(
 
 //Token Control
 app.put(
-    "session/control/:EMailAddress",
+    "/session/control/:EMailAddress",
     EMailAddressControl,
     AuthControl,
     AuthenticateJWTToken,
